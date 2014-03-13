ff475f9563a17ec0ff4faffdc23188162bf78ded
pixelshade <psuldo@gmail.com>
2014-03-13 22:56:27 +0100

    Ugly bug fix - tabs and map recreation
    when was map recreated, there was problem with duplicate ids, solved,
    thanks stackoverflow

--- client/gpsoclient/src/main/java/shade/pixel/gpsoclient/MyMapFragment.java
+++ client/gpsoclient/src/main/java/shade/pixel/gpsoclient/MyMapFragment.java
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
@@ -13,16 +15,35 @@ import com.google.android.gms.maps.SupportMapFragment;
  * Created by pixelshade on 11.3.2014.
  */
 public class MyMapFragment extends Fragment {
-    GoogleMap map;
+    private GoogleMap map;
+    private static View view;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        } else {
+            view = inflater.inflate(R.layout.fragment_map, container, false);
+        }
 
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        initilizeMap();
 
-        map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        map.setMyLocationEnabled(true);
-        return rootView;
+        return view;
     }
+
+    /**
+     * set up the map
+     */
+    private void initilizeMap() {
+            map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.fragment_map)).getMap();
+            if (map == null) {
+                Toast.makeText(getActivity(), "Unable to create map", Toast.LENGTH_SHORT).show();
+            } else {
+                map.setMyLocationEnabled(true);
+                map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+            }
+    }
+
 }
